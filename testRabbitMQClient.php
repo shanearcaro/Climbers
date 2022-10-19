57aefd5e77534ff464039923d32e62a3b632ddc7
Eric Margadonna <eric082659@gmail.com>
2022-10-19 11:07:40 -0400

    Fixed the client test php file

--- testRabbitMQClient.php
+++ testRabbitMQClient.php
@@ -1,7 +1,5 @@
 #!/usr/bin/php
 <?php
-require_once('path.inc');
-require_once('get_host_info.inc');
 require_once('rabbitMQLib.inc');
 
 $client = new rabbitMQClient("testRabbitMQ.ini","testServer");
