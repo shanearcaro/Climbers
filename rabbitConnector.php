271f8bcded2948f82a1b3676090291fb60b34a6d
ericmargadonna <eric082659@gmail.com>
2022-10-20 00:19:26 -0400

    changed output of rabbitConnector

--- rabbitConnector.php
+++ rabbitConnector.php
@@ -24,7 +24,7 @@ $response = $client->send_request($request);
 //$response = $client->publish($request);
 
 //echo "client received response: ".PHP_EOL;
-echo $response;
+echo $response["returnCode"];
 //echo "\n\n";
 
 //echo $argv[0]." END".PHP_EOL;
