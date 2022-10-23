33fdfea235776fdacec9ec4d08161f01b48b27ce
Eric Margadonna <eric082659@gmail.com>
2022-10-23 01:28:00 -0400

    the API query works!

--- dmz/fetchData.py
+++ dmz/fetchData.py
@@ -1,21 +1,16 @@
 import requests as r
 import json
 
-reqdict = {"query MyQuery":
-            {"areas":
-                ['area_name','uuid']
-            }
-          }
-
-{"query":"query Example1 {\n  areas {\n    area_name\n    uuid\n  }\n}\n\nquery Ex2 {\n  area(uuid: \"c441f90b-7951-5ff1-b10a-e559fe24d2bc\") {\n    id\n    totalClimbs\n  }\n}\n","variables":null,"operationName":"Ex2"}
-
-json_dict = json.dumps(reqdict)
+query = ''' query {
+    areas {
+        area_name
+        uuid
+    }
+}'''
 
 myreq = r.post('https://stg-api.openbeta.io', 
                 headers={'content-type': 'application/json'},
-                json=json_dict,
+                json={'query': query},
             )
 
-print(json_dict)
-print("\n------------------------------\n")
 print(myreq.text)
