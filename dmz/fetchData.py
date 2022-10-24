94152dec505dc6d9eb265a60636ba20b34de3f4b
= <=>
2022-10-24 15:53:29 -0400

    Made the api request more useful, still not great. READ COMMENTS

--- dmz/fetchData.py
+++ dmz/fetchData.py
@@ -1,16 +1,36 @@
 import requests as r
 import json
 
+#THIS QUERY IS INCOMPLETE
+#IT WILL RETURN SOME BROKEN DATA THAT WILL NEED TO BE FILTERED OUT
+#IT BOILS DOWN TO REMOVING ANY AREAS WITH NO CLIMBS
 query = ''' query {
-    areas {
-        area_name
-        uuid
+  areas(filter: {area_name: {match: "New Jersey"}}) {
+    children {
+      children {
+        children {
+          areaName
+          climbs {
+            name
+          }
+          metadata {
+            lat
+            lng
+          }
+        }
+      }
     }
-}'''
+  }
+}
+'''
 
-myreq = r.post('https://stg-api.openbeta.io', 
+myreq = r.post('https://api.openbeta.io', 
                 headers={'content-type': 'application/json'},
                 json={'query': query},
             )
 
-print(myreq.text)
+#This is a dictionary containing the entire response
+#Do with it what you will
+jdict = json.loads(myreq.text)
+
+#print(jdict)
