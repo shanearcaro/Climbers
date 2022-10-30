1a84d975ce71453806b59e3eb0ab6b0c36c4c792
ericmargadonna <eric082659@gmail.com>
2022-10-30 02:25:49 -0400

    added lots of comments

--- dmz/fetchData.py
+++ dmz/fetchData.py
@@ -1,29 +1,119 @@
 import requests as r
 import json
-from query import querystring
 from pprint import pprint
 
-# This is a recursive function that will return a dictionary 
-# of the area names and lat/lng coordinates of the bottom-most 
-# areas in the response
-def get_lowest_area_dicts(data):
-    area_dicts = []
-    for area_dict in data:
-        if area_dict['children'] == []:
-            area_dicts.append(area_dict)
-        else:
-            area_dicts.extend(get_lowest_area_dicts(area_dict['children']))
-    return area_dicts
+q_nj_areas = ''' 
+fragment locationData on Area{
+  metadata {
+    lat
+    lng
+  }
+}
 
+query NJAreasWithLatLng{
+  areas(filter: {area_name: {match: "New Jersey"}}){
+    children {
+      uuid
+      areaName
+      ...locationData
+      children {
+        uuid
+        areaName
+        ...locationData
+        children {
+          uuid
+          areaName
+          ...locationData
+          children {
+            uuid
+            areaName
+            ...locationData
+            children{
+              uuid
+              areaName
+              ...locationData
+              children{
+              	uuid
+                areaName
+              	...locationData
+                children{
+              	  uuid
+                  areaName
+              	  ...locationData
+                  children{
+              	    uuid
+                    areaName
+              	    ...locationData
+                  }
+                }
+              }
+            }
+          }
+        }
+      }
+    }
+  }
+}
+'''
+
+querystring2 = '''
+fragment climbData on Area{
+  totalClimbs
+  climbs {
+    id
+    fa
+    yds  
+    content{
+      description
+      protection
+    }
+  }
+}
+
+query SingleArea{
+  area(uuid: ""){
+    ...climbData
+  }
+}
+'''
+
+def get_lowest_area_dicts(area_dicts_list):
+  '''
+  Recursive function that takes a list of nestested dictionaries, 
+  and traverses through them to return a list of dictionaries with
+  the area names and other data of the bottom-most areas in the response
+  '''
+  #Setup the return list
+  bottom_area_dicts = []
+
+  #Iterate through the passed in list of dictionaries.
+  #If the current dictionary has an empty list for the 'children' key,
+  #then it is a bottom-most area.
+  #Otherwise, the current area is not a bottom-most area, so we need 
+  #to recurse and check the areas under (children of) it.
+  for area_dict in area_dicts_list:
+    if area_dict['children'] == []:
+      bottom_area_dicts.append(area_dict)
+    else:
+      bottom_area_dicts.extend(get_lowest_area_dicts(area_dict['children']))
+
+  #Return the list of bottom-most area dictionaries
+  return bottom_area_dicts
+
+#Build the request
 myreq = r.post('https://api.openbeta.io', 
                 headers={'content-type': 'application/json'},
-                json={'query': querystring},
+                json={'query': q_nj_areas},
             )
 
 # This is a dictionary containing the entire response
 responseDictionary = json.loads(myreq.text)
+
+#So we start by peeling off the outer layers
 responseDictionary = responseDictionary['data']['areas'][0]['children']
 
-#pprint(responseDictionary)
+#Then we can run our facny recursive function
+lowest_areas = get_lowest_area_dicts(responseDictionary)
 
+#pprint(responseDictionary)
 pprint(get_lowest_area_dicts(responseDictionary))
