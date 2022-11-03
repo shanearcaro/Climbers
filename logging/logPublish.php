fb64c6ce5cdab6a2d860fd0d156ac0360da27c0b
ericmargadonna <eric082659@gmail.com>
2022-11-02 23:06:34 -0400

    Added function version of logPublish for inclusion in other scripts

--- logging/logPublish.php
+++ logging/logPublish.php
@@ -8,18 +8,32 @@ require_once('../djmagic/rabbitMQLib.inc');
 //This is for cross VM communication
 $client = new rabbitMQClient("../config/logConfig.ini","testServer");
 
-//There should always be 2 arguments, the script name and the log message
-if($argc != 2){
-	echo "Incorrect number of arguments!".PHP_EOL."Usage: logPublish.php <log_message>".PHP_EOL.PHP_EOL;
-	exit();
+//This is a function-ized version of the code below that is used by
+//server scripts to send messages to the logging exchange
+function processLog($message){
+	//Build the request
+	$request['message'] = $message;
+	//Publish the log message
+	$response = $client->publish($request);
+	return $response;
 }
 
-//Build the request
-$request['message'] = $argv[1];
+//This checks if the script is being run directly or if it's being 
+//included by another script
+if(get_included_files()[0] == __FILE__) {
+	//There should always be 2 arguments, the script name and the log message
+	if($argc != 2){
+		echo "Incorrect number of arguments!".PHP_EOL."Usage: logPublish.php <log_message>".PHP_EOL.PHP_EOL;
+		exit();
+	}
 
-//Publish the log message
-$response = $client->publish($request);
+	//Build the request
+	$request['message'] = $argv[1];
 
-//echo "client received response: ".PHP_EOL;
-echo $response.PHP_EOL;
+	//Publish the log message
+	$response = $client->publish($request);
+
+	//echo "client received response: ".PHP_EOL;
+	echo $response.PHP_EOL;
+}
 ?>
