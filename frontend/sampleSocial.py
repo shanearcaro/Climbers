05d5e0114794c2a8fadb528d9230402b811c641c
Richie Temple <richie@Nickies-MacBook-Air.local>
2022-11-07 12:05:34 -0500

    samplesocial

---
+++ frontend/sampleSocial.py
@@ -0,0 +1,74 @@
+import dash
+from dash import Dash, html, dcc, callback, Input, Output, State, no_update
+import pandas as pd
+import plotly.figure_factory as ff
+import plotly.express as px
+import plotly.graph_objects as go
+import requests as r
+import json
+import datetime as dt
+from datetime import date
+
+#------This-is-to-be-replaced-by-api/dmz-functionality-------\
+app = Dash(__name__, update_title='', suppress_callback_exceptions=True)
+
+# Get raw data for graph (formatted raw dictionaries)
+
+# App layout
+app.layout = html.Div([
+  html.Div([
+      html.Div([
+        'friends'
+      ], style={
+      'margin':'10px',
+      'height':'90%',
+      'border':'1px solid black'
+    }),
+  ], style={
+      'width':'90%',
+      'height':'90%'
+    }),
+  html.Div([
+      'hi'
+  ], style={
+      'width':'90%',
+      'height':'90%'
+    }),
+], style={
+  'display':'grid',
+  'grid-template-columns':'auto auto',
+  'width':'95%',
+  'height':'95%'
+})
+
+@app.callback(
+    Output('area', 'children'),
+    Output('lat', 'children'),
+    Output('lon', 'children'),
+    Input('map', 'clickData'),
+    prevent_initial_call=True
+)
+def click(clickdata):
+    print(clickdata)
+    points = clickdata['points'][0]
+    print(points['lat'])
+    print(points['lon'])
+    print(points['customdata'][0])
+    print(points)
+    return points['customdata'][0], points['lat'], points['lon']
+  
+@app.callback(
+    Output('output-container-date-picker-single', 'children'),
+    Input('my-date-picker-single', 'date'),
+    State('time-dropdown', 'value'),
+    prevent_initial_call=True)
+def update_output(date_value, time):
+    string_prefix = 'You have selected: '
+    if time is not None:
+      if date_value is not None:
+          date_object = date.fromisoformat(date_value)
+          date_string = date_object.strftime('%B %d, %Y')
+          return string_prefix + date_string + ' at ' + time
+
+if __name__ == '__main__':
+    app.run_server(host="0.0.0.0", port="8050", debug=True)
