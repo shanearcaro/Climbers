d311bb1dc28e1cf91ec305044bf41ea04611acaf
Eric Margadonna <eric082659@gmail.com>
2022-11-14 11:42:07 -0500

    Really fixed delete

--- dmz/getLowestAreas.py
+++ dmz/getLowestAreas.py
@@ -9,6 +9,7 @@ fragment locationData on Area{
     lng
   }
 }
+
 query NJAreasWithLatLng{
   areas(filter: {area_name: {match: "New Jersey"}}){
     children {
@@ -55,8 +56,6 @@ query NJAreasWithLatLng{
 }
 '''
 
-
-
 def get_lowest_area_dicts(area_dicts_list):
   '''
   Recursive function that takes a list of nestested dictionaries, 
@@ -72,10 +71,11 @@ def get_lowest_area_dicts(area_dicts_list):
   #Otherwise, the current area is not a bottom-most area, so we need 
   #to recurse and check the areas under (children of) it.
   for area_dict in area_dicts_list:
-    if area_dict['children'] == []:
+    if area_dict.get('children') == [] and area_dict.get('uuid') is not None:
       bottom_area_dicts.append(area_dict)
-    else:
-      bottom_area_dicts.extend(get_lowest_area_dicts(area_dict['children']))
+    elif area_dict.get('children') != []:
+      bottom_area_dicts.extend(get_lowest_area_dicts(area_dict.get('children')))
+    continue
 
   #Return the list of bottom-most area dictionaries
   return bottom_area_dicts
@@ -89,14 +89,19 @@ myreq = r.post('https://api.openbeta.io',
 # This is a dictionary containing the entire response
 responseDictionary = json.loads(myreq.text)
 
-
 #So we start by peeling off the outer layers
 responseDictionary = responseDictionary['data']['areas']
-print(responseDictionary)
 
 #Then we can run our facny recursive function
-#lowest_areas = get_lowest_area_dicts(responseDictionary)
+lowest_areas = get_lowest_area_dicts(responseDictionary)
 
-#print(lowest_areas)
-#pprint(responseDictionary)
-#pprint(get_lowest_area_dicts(responseDictionary))
+lowest_areas_formatted= []
+for area_dict in lowest_areas:
+  temp_dict = {'uuid': area_dict.get('uuid'),
+               'areaName': area_dict.get('areaName'),
+               'lat': area_dict.get('metadata').get('lat'),
+               'lng': area_dict.get('metadata').get('lng'),
+               'children': area_dict.get('children')}
+  lowest_areas_formatted.append(temp_dict)
+
+print(json.dumps(lowest_areas_formatted))
