d1f5cf4840e7ee8ba5e226bc8bbb8561273d8974
saphire001 <spj29@njit.edu>
2022-12-07 17:48:18 +0000

    instructions to start-server.sh added

--- bash/SysD.sh
+++ bash/SysD.sh
@@ -7,3 +7,19 @@ sudo update-rc.d mysql enable
 sudo update-rc.d rabbitmq-server enable
 
 sudo systemctl list-unit-files --type=service --state=enabled --all
+
+#! instructions on how to make start-server.sh run on boot 
+sudo nano /etc/systemd/system/start-server.service
+
+[Unit]
+Description=Start Services 
+
+[Service] 
+ExecStart= location of .sh file 
+
+[Install] 
+WantedBy=multi-user.target
+
+systemctl status start-server.service
+
+sudo systemctl enable start-server.service
