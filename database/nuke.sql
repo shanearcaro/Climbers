72dcfd299d49d9734bacfe11e2ef3a2427240b90
Shane Arcaro <smarcaro@gmail.com>
2022-12-12 08:28:19 -0500

    Adding a nuke script for deleting all tables without causing a violation

---
+++ database/nuke.sql
@@ -0,0 +1,9 @@
+-- Drop all table without causing an integrity violtation
+
+DROP TABLE IF EXISTS ChatMembers;
+DROP TABLE IF EXISTS ChatMessages;
+DROP TABLE IF EXISTS Chats;
+DROP TABLE IF EXISTS UserBlocks;
+DROP TABLE IF EXISTS UserFriends;
+DROP TABLE IF EXISTS UserStats;
+DROP TABLE IF EXISTS Users;
