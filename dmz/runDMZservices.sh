a0d65ebb437c804f3ae93789297d58d7c411fd2b
ericmargadonna <eric082659@gmail.com>
2022-12-17 21:30:34 -0500

    Broke some stuff so that other stuff works

--- dmz/runDMZservices.sh
+++ dmz/runDMZservices.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 # This script is used to run the DMZ services
-cd ../logging/
-./logListen.php &
-cd ../dmz/
+# cd ../logging/
+# ./logListen.php &
+# cd ../dmz/
 ./apiServer.php
--- frontend/runWebServer.sh
+++ frontend/runWebServer.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 # This script is used to run the web server
-cd ../logging/
-./logListen.php &
-cd ../frontend/
+# cd ../logging/
+# ./logListen.php &
+# cd ../frontend/
 python3 app.py
